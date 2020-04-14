97aecb93f6ac59ba232e09e6b6510f91c18ed6ce
Alexeik <evaldezs1200@alumno.ipn.mx>
2020-04-14 00:20:15 -0500

    Se hizo la carga de nuevas dependencias para esto se pueden dos tipos uno en package.json en dependencias o en linea de comandos del proyecto npm i "NOMBREdependencia". Tambien se hizo uso de la importacion de datos rest de un portal de pruebas: https://jsonplaceholder.typicode.com

--- src/components/User.vue
+++ src/components/User.vue
@@ -50,6 +50,10 @@
       deleteUser(user){
         this.users.splice(this.users.indexOf(user),1);
       }
+    },
+    created(){
+      this.$http.get('https://jsonplaceholder.typicode.com/users')
+        .then(res=> this.users=res.body);
     }
   }
 </script>
